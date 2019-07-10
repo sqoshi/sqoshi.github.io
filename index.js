eb5f8a6e778c7ca85e389849a21c4c4de829456e
sqoshi <piotrpopis98@icloud.com>
2019-07-10 13:52:52 +0200

    about making me

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const hamburger = document.querySelector(".hamburger");
 const dog = document.querySelector(".dog");
-const row = document.querySelector(".row");
-const column = document.querySelector(".column");
-const first = document.querySelector(".first");
-
 const links = document.querySelectorAll(".dog li");
 
 hamburger.addEventListener('click', () => {
-  dog.classList.toggle("open");
 
+  dog.classList.toggle("open");
 });
+
+
+
+const row = document.querySelector(".row");
+const column = document.querySelector(".column");
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const third = document.querySelector(".third");
+
 first.addEventListener('click', () => {
   window.open('https://github.com/sqoshi/AKISO/blob/master/L4/z3.c',
   'blank');
 });
 second.addEventListener('click', () => {
-  window.open('https://github.com/sqoshi/AKISO/TS',
+  window.open('https://github.com/sqoshi/TS',
   'blank');
 });
 third.addEventListener('click', () => {
