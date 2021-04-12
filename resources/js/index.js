abe085c451280539b11d0f0c34343f6074142771
piotr <piotrpopis98@icloud.com>
2021-04-12 22:36:03 +0200

    no js browser handling + jslint

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,13 @@
+/*jslint devel: true, browser: true, white: true */
+
+/*property
+    backgroundSize, getElementById, length, maxHeight, scrollHeight, src, style
+*/
+
+var x = -1;
+
 function toggle_menu() {
-  var coll = document.getElementById("menu-icon");
-  var i;
-  var content = document.getElementById('menu-links');
+  var content = document.getElementById("menu-links");
   if (content.style.maxHeight) {
     content.style.maxHeight = null;
   } else {
@@ -11,31 +17,32 @@ function toggle_menu() {
 
 
 function displayNextImage() {
-  x = (x === images.length - 1) ? 0 : x + 1;
-  document.getElementById("project-img").style.backgroundSize = 'cover'
-  document.getElementById("project-img").src = "resources/img/projects/" + images[x];
+  x = ((x === images.length - 1) ? 0 : x + 1);
+  var img_el = document.getElementById("project-img");
+  img_el.style.backgroundSize = "cover";
+  img_el.src = "resources/img/projects/" + images[x];
 }
 
 function displayPreviousImage() {
-  x = (x <= 0) ? images.length - 1 : x - 1;
-  document.getElementById("project-img").style.backgroundSize = 'cover'
-  document.getElementById("project-img").src = "resources/img/projects/" + images[x];
+  x = ((x <= 0) ? images.length - 1 : x - 1);
+  var img_el = document.getElementById("project-img");
+  img_el.style.backgroundSize = "cover";
+  img_el.src = "resources/img/projects/" + images[x];
 }
 
 function startTimer() {
-  setInterval(displayNextImage,3000);
+  setInterval(displayNextImage, 3000);
 }
 
 var images = [
-  'nnb.png',
-    'csa.png',
-    'gwi.png',
-    'shopping_bot.png',
-    'announce_flask.png',
-    'gogame_java.png',
-    'scrabble_hack.png',
-    'gpa.png',
-    'woe.jpg',
-    'portfolio.png',
-  ],
-  x = -1;
+  "nnb.png",
+  "csa.png",
+  "gwi.png",
+  "shopping_bot.png",
+  "announce_flask.png",
+  "gogame_java.png",
+  "scrabble_hack.png",
+  "gpa.png",
+  "woe.jpg",
+  "portfolio.png"
+];
--- resources/js/projects.js
+++ resources/js/projects.js
@@ -1,5 +1,13 @@
+/*jslint devel: true, browser: true, white: true */
+
+/*property
+    bottom, getElementById, maxHeight, parentNode, querySelector, scrollHeight,
+    style, visibility
+*/
+
+
 function toggle_menu() {
-  var content = document.getElementById('menu-links');
+  var content = document.getElementById("menu-links");
   if (content.style.maxHeight) {
     content.style.maxHeight = null;
   } else {
@@ -9,17 +17,14 @@ function toggle_menu() {
 
 function toggle_project_info(element) {
   var parent = element.parentNode.parentNode;
-  var content = parent.querySelector('.project-info');
+  var content = parent.querySelector(".project-info");
   if (content.style.maxHeight) {
-        parent.style.bottom = null;
-    content.style.visibility = 'collapse';
+    parent.style.bottom = null;
+    content.style.visibility = "collapse";
     content.style.maxHeight = null;
   } else {
-    parent.style.bottom = '-5rem';
-    content.style.visibility = 'visible';
+    parent.style.bottom = "-5rem";
+    content.style.visibility = "visible";
     content.style.maxHeight = content.scrollHeight + "px";
-
   }
 }
-
-file_path = 'file:///home/piotr/Documents/studies/modern_www_technologies/portfolio/resources/test.html'
